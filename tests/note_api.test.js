2c3158a8de97e5af243f57d63672a3d8035471a7
Cruz <cruz.tag@gmail.com>
2021-12-07 16:05:53 -0600

    adde test for add users

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -3,7 +3,8 @@ const supertest = require('supertest');
 const helper = require('./test_helper.js');
 const app = require('../app');
 const api = supertest(app);
-
+const bcrypt = require('bcrypt');
+const User = require('../models/user.js');
 const Note = require('../models/note');
 
 beforeEach(async () => {
@@ -113,6 +114,61 @@ describe('deletion of a note', () => {
 
     expect(contents).not.toContain(noteToDelete.content);
   });
+});
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash('sekret', 10);
+    const user = new User({ username: 'root', passwordHash });
+
+    await user.save();
+  });
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti  luukkainen',
+      password: 'salainen',
+    };
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    console.log(usernames);
+    expect(usernames).toContain(newUser.username);
+  });
+
+  // test('creation fails with proper statuscode and message', async () => {
+  //   const usersAtStart = await helper.usersInDb();
+
+  //   const newUser = {
+  //     username: 'root',
+  //     name: 'Superuser',
+  //     password: 'salainen',
+  //   };
+
+  //   const result = await api
+  //     .post('/api/users')
+  //     .send(newUser)
+  //     .expect(400)
+  //     .expect('Content-type', /application\/json/);
+
+  //   expect(result.body.error).toContain('`username` to be unique');
+
+  //   const usersAtEnd = await helper.usersInDb();
+  //   expect(usersAtEnd).toHaveLength(usersAtStart.length);
+  // });
 });
 
 afterAll(() => {

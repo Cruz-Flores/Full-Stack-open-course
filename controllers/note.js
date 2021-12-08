1a64875127d76323ab702c691e0c941a712ee9f9
Cruz <cruz.tag@gmail.com>
2021-12-07 21:17:05 -0600

    added function to add note with token

--- controllers/note.js
+++ controllers/note.js
@@ -1,4 +1,5 @@
 const notesRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Note = require('../models/note');
 const User = require('../models/user.js');
 
@@ -16,16 +17,28 @@ notesRouter.get('/:id', async (request, response, next) => {
   }
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body;
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   /* if (body.content === undefined) {
     return response.status(400).json({
       error: 'content missing',
     });
   } */ // redundant???????
-
-  const user = await User.findById(body.userId);
 
   const note = new Note({
     content: body.content,

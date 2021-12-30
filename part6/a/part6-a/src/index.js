84fb60a34273ff8aeb9aeb9e32f49cdb407b9021
Cruz <cruz.tag@gmail.com>
2021-12-29 22:59:09 -0600

    part6a

---
+++ part6/a/part6-a/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './App.js';
+import noteReducer from './reducers/noteReducer.js';
+
+const store = createStore(noteReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
